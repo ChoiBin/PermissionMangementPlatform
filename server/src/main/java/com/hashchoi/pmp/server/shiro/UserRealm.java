9d5d96637d65453feb463b278a026686b5bd8b53
ChoiBin <841494724@qq.com>
2019-09-14 21:57:50 +0800

    补充README.md中的图片

--- server/src/main/java/com/hashchoi/pmp/server/shiro/UserRealm.java
+++ server/src/main/java/com/hashchoi/pmp/server/shiro/UserRealm.java
@@ -15,7 +15,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.rmi.activation.UnknownObjectException;
 
 /**
  * @author : choibin
